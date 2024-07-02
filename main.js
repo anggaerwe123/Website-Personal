5d86bd3b69786cfd3cc7f3b524015894bb7f7609
anggaerwe123 <anggarizkiwijayaa@gmail.com>
2024-07-02 12:10:49 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+const nohp = '62895622192248';
+let pesan = 'https://api.whatsapp.com/send?phone='  + nohp  + '&text=Assalamualaikum, saya ingin join ke group Bhakti Jaya Gemilang.';
+
+
+document.querySelector('.btnJoin').href = pesan;
